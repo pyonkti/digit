a7c0fd4c845e40b96a91990285ef90766812e762
pyonkti <751069606@qq.com>
2024-07-04 19:21:42 +0200

    vibration detect

--- demo2.py
+++ demo2.py
@@ -6,6 +6,31 @@ from digit_interface import DigitHandler
 import cv2
 import numpy as np
 
+class FIFOQueue:
+    def __init__(self, size):
+        self.queue = []
+        self.size = size
+    
+    def enqueue(self, item):
+        if len(self.queue) >= self.size:
+            self.dequeue()  # Remove the oldest item to make space
+        self.queue.append(item)
+    
+    def dequeue(self):
+        if self.queue:
+            return self.queue.pop(0)
+        else:
+            return None  # or raise an exception
+    
+    def __str__(self):
+        return str(self.queue)
+    
+    def __len__(self):
+        return len(self.queue)
+    
+    def clear(self):
+        self.queue = []
+
 def remove_vertical_lines(lines):
     filtered_lines = []
     if lines is not None:
@@ -35,7 +60,7 @@ def calculate_rho_theta(x1, y1, x2, y2):
 
     return rho, theta
 
-def draw_line(lines, grey_image):
+def draw_line(lines, frame, edges):
     try:
         # Find the longest line segment (or any other criteria)
         longest_line = max(lines, key=lambda line: np.linalg.norm((line[0][2] - line[0][0], line[0][3] - line[0][1])))
@@ -46,7 +71,7 @@ def draw_line(lines, grey_image):
         a = math.cos(theta)
         b = math.sin(theta)
 
-        length = max(grey_image.shape)  # Use the maximum dimension of the image
+        length = max(frame.shape)  # Use the maximum dimension of the image
         x0 = (x1 + x2) / 2
         y0 = (y1 + y2) / 2
         pt1 = (int(x0 + length * (-b)), int(y0 + length * (a)))
@@ -54,39 +79,56 @@ def draw_line(lines, grey_image):
 
         if b!= 0:
             parallelogram_points = np.array([
-                [0, int((rho-20)/b)],
-                [0, int((rho+20)/b)],
-                [480, int((rho-480*a+20)/b)],
-                [480, int((rho-480*a-20)/b)]
+                [0, int((rho-10)/b)],
+                [0, int((rho+10)/b)],
+                [480, int((rho-480*a+10)/b)],
+                [480, int((rho-480*a-10)/b)]
             ])
         else:
             parallelogram_points = np.array([
-                [0, rho-20],
-                [0, rho+20],
-                [480, rho-20],
-                [480, rho+20]
+                [0, rho-10],
+                [0, rho+10],
+                [480, rho-10],
+                [480, rho+10]
             ])
-        cv2.polylines(grey_image, [parallelogram_points], isClosed=True, color=(0, 255, 0), thickness=2)
+        draw_parallelogram(parallelogram_points, frame)
+        draw_parallelogram(parallelogram_points, edges)
         # Draw the longest line
-        cv2.line(grey_image, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
+        cv2.line(frame, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
     except TypeError as e:
         print(f"An error occurred while drawing lines: {e}")
+    return parallelogram_points
+
+def draw_parallelogram(parallelogram_points, image):
+    try:
+        cv2.polylines(image, [parallelogram_points], isClosed=True, color=(255, 255, 255), thickness=2)
+    except TypeError as e:
+        print(f"An error occurred while drawing parallelogram_: {e}")
+
+def count_edge_pixels_in_parallelogram(edges, vertices):
+    mask = np.zeros_like(edges)
+    cv2.fillPoly(mask, [vertices], 255)
+    edge_pixels = cv2.countNonZero(cv2.bitwise_and(edges, mask))
+    total_pixels = cv2.countNonZero(mask)
+    return float(edge_pixels/total_pixels)
 
 def process_continuous_frames(d):
     """
     Continuously captures and processes frames from the DIGIT device.
     """
 
-    gaussian = 13
-    median = 9
-    canny_threshold1=44
-    canny_threshold2=51
-    hough_rate = 41
-    #line_threshold = 20
+    gaussian = 23
+    median = 5
+    canny_threshold1=160
+    canny_threshold2=183
+    hough_rate = 44
+    #line_threshold = 10
     break_rate = 35
     threshold_increment = 1  # How much to change the threshold by in each iteration
-    minLineLength= 100
-    maxLineGap= 35
+    minLineLength= 117
+    maxLineGap= 51
+    parallelogram_points = None
+    edge_rate_queue = FIFOQueue(size=10)
 
     #skip first 20 frames for camera to adjust its white balance
     for _ in range(20):
@@ -113,6 +155,19 @@ def process_continuous_frames(d):
             # Canny Edge Detection
             edges = cv2.Canny(image=blurred_image, threshold1=canny_threshold1, threshold2=canny_threshold2) # Canny Edge Detection
 
+            if parallelogram_points is not None:
+                rate = count_edge_pixels_in_parallelogram(edges,parallelogram_points)
+                if  edge_rate_queue.__len__() < 10:
+                    edge_rate_queue.enqueue(rate)
+                else:
+                    mean_rate = np.mean(np.array(edge_rate_queue.queue))
+                    change_rate = abs((rate-mean_rate)/mean_rate)
+                    if change_rate >= 0.5:
+                        print('vibrated')
+                        edge_rate_queue.clear()
+                        parallelogram_points = None                        
+                    else:
+                        edge_rate_queue.enqueue(rate)
 
             # First attempt to find lines with initial rate
             lines = cv2.HoughLinesP(edges, 1, np.pi / 180, temp_hough, None, minLineLength, maxLineGap)
@@ -127,10 +182,10 @@ def process_continuous_frames(d):
                     if len(lines) == 0:
                         continue
                     else:
-                        draw_line(lines, frame)
+                        parallelogram_points = draw_line(lines, frame, edges)
                         break
             else:
-                draw_line(lines, frame)
+                parallelogram_points = draw_line(lines, frame, edges)
 
             # If more than 3 lines are found, try to narrow it down by increasing the threshold
             #if found_lines and len(lines) > line_threshold:
@@ -139,14 +194,14 @@ def process_continuous_frames(d):
                     #temp_lines = cv2.HoughLinesP(edges, 1, np.pi / 180, temp_hough, None, minLineLength, maxLineGap)
                     #temp_lines = remove_vertical_lines(temp_lines)
                     #if len(temp_lines) <= 0:
-                        #draw_line(lines, frame)
+                        #parallelogram_points = draw_line(lines, frame, edges)
                         #break
                     #elif 0 < len(temp_lines) < line_threshold:
                         #lines = temp_lines  # Update lines with the filtered results
-                        #draw_line(lines, frame)
+                        #parallelogram_points = draw_line(lines, frame, edges)
 
             #elif found_lines and 0 < len(lines) < line_threshold:
-                #draw_line(lines, frame)
+                #parallelogram_points = draw_line(lines, frame, edges)
 
             tiled_layout = np.zeros((height, width * 3, channels), dtype=np.uint8)
 
